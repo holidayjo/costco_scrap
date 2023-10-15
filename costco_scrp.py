31a6822a32a3b1ce7c1bace5de7aeedaccb33a80
holidayjo <ossam1996@gmail.com>
2023-10-15 11:09:18 +0900

    selenium solving

--- costco_scrp.py
+++ costco_scrp.py
@@ -1,15 +1,12 @@
 # this repository is based on the website of https://www.blog.datahut.co/post/scrape-product-information-from-costco-using-python
 
-import pandas as pd
-import lxml
-import bs4
-from selenium import webdriver
+# Importing necessary libraries
 
+import pandas as pd
+from lxml import etree as et
+from bs4 import BeautifulSoup
+from selenium import webdriver
 pd.options.mode.chained_assignment = None
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-
-
-
-
+driver = webdriver.Chrome()
