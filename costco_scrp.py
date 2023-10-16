d77f3a66812188185c6aeaf69f4581f524e6f0c2
holidayjo <ossam1996@gmail.com>
2023-10-16 14:09:02 +0900

    231016

--- costco_scrp.py
+++ costco_scrp.py
@@ -10,3 +10,21 @@ pd.options.mode.chained_assignment = None
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+print(driver)
+
+def extrac_content(url):
+    # Function to extract content from page.
+    driver.get(url)
+    page_content = driver.page_source
+    soup = BeautifulSoup(page_content, 'html.parser')
+    return soup
+
+def click_url(driver):
+    # Function to click e electronic category 'Audio/Video' and extract content from the page.
+    driver.find_element(By.XPATH, '//*[@id="navpills-sizing/a[3]').click()
+    html_content = driver.page_source
+    soup         = BeautifulSoup(html_content, 'html.parser')
+    return
+
+def category_link(soup):
+    
