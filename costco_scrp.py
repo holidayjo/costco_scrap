f0805e8408274b9698b7a9ab46d5ed77ca4a706d
Holiday_Jo <ossam1996@gmail.com>
2023-10-22 22:08:55 +0900

    Update costco_scrp.py

--- costco_scrp.py
+++ costco_scrp.py
@@ -1,13 +1,10 @@
 # this repository is based on the website of https://www.blog.datahut.co/post/scrape-product-information-from-costco-using-python
 
 # Importing necessary libraries
-
+import json
 import pandas as pd
 from lxml import etree as et
 from bs4 import BeautifulSoup
-import selenium
-import webdriver_manager
-
 pd.options.mode.chained_assignment = None
 from selenium.webdriver.common.by import By
 
@@ -15,10 +12,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options = webdriver.ChromeOptions()
-driver         = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
-driver.get('http://www.google.com')
+# driver.get('http://www.google.com')
+def url_reader(menu_name='whats_new'):
+    with open('/media/holidayj/My Documents/Github/Python/webscr/costco_scrap-2/urls.json') as f:
+        data = json.load(f)
+    f.close()
+    return data[menu_name]
 
 def extract_content(url):
     # Function to extract content from page.
@@ -27,9 +26,9 @@ def extract_content(url):
     soup = BeautifulSoup(page_content, 'html.parser')
     return soup
 
-def click_url(driver):
+def click_url(driver, click_path):
     # Function to click e electronic category 'Audio/Video' and extract content from the page.
-    driver.find_element(By.XPATH, '//*[@id="navpills-sizing/a[3]').click()
+    driver.find_element(By.XPATH, click_path).click()
     html_content = driver.page_source
     soup         = BeautifulSoup(html_content, 'html.parser')
     return soup
@@ -40,3 +39,19 @@ def category_link(soup):
     for div in soup.find_all('div', attrs = {"class": "col=-xs-12 col-lg-6 con-cl-3"}):
         break
         
+        
+    
+        
+# Reading urls and html links.
+root_url   = url_reader(menu_name='root')
+click_1 = url_reader(menu_name='whats_new')
+print(root_url)
+print(click_1)
+
+
+chrome_options = webdriver.ChromeOptions()
+driver         = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+driver.get(root_url)
+url_content = click_url(driver, click_1)
+
