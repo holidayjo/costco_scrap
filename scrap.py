d1ddb5d2417f9dade74f4fa53a50357c5644d23b
holidayjo <ossam1996@gmail.com>
2025-06-20 22:39:37 +0900

    20250620

--- scrap.py
+++ scrap.py
@@ -2,15 +2,26 @@ from playwright.sync_api import sync_playwright
 import pandas as pd
 
 with sync_playwright() as p:
+    print("sync_playwright() Begin!")
     browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-
+    page    = browser.new_page()
+    
     # Load the page (relax wait condition)
     page.goto("https://www.costco.co.kr/c/whatsnew", wait_until="domcontentloaded")
     page.wait_for_timeout(3000)  # Adjust as needed
+    
+    for _ in range(5):
+        page.mouse.wheel(0, 2000)
+        page.wait_for_timeout(1000)
+        
+    html = page.content()
+    with open("debug_page.html", "w", encoding="utf-8") as f:
+        f.write(html)
+        
 
     # Extract product items
     products = page.query_selector_all("div.product-tile-set")
+    print(products)
 
     items = []
     for product in products:
